648a4efc80b8f076cdc8abd72daa56f01a543bff
Crosson David <crosson.david@gmail.com>
2019-12-08 09:33:56 +0100

    Day7 is ~OK

--- src/main/scala/adventofcode/Day7.scala
+++ src/main/scala/adventofcode/Day7.scala
@@ -10,6 +10,10 @@ import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+
 
 object Day7 {
 
@@ -128,119 +132,7 @@ object Day7 {
 
   // ========================================================================================
 
-/*
   object Part2 {
-
-    object ProgramActor {
-      sealed trait ProgramMessage
-
-      case class Setup(linkedTo:ActorRef[Input]) extends ProgramMessage
-      case class Input(value:Int) extends ProgramMessage
-
-      def init(code: Vector[Int]):Behavior[ProgramMessage] = {
-        Behaviors.receiveMessage { setup: Setup =>
-          val replyTo = setup.linkedTo
-          run(code, replyTo)
-        }
-      }
-
-      def run(code: Vector[Int], replyTo:ActorRef[Input], pointer:Int=0, inputs:Vector[Int]=Vector.empty): Behavior[ProgramMessage] =
-          Behaviors.receiveMessage { message:Input =>
-            process(code, replyTo, pointer, inputs :+ message.value)
-        }
-
-      def process(code: Vector[Int], replyTo:ActorRef[Input], pointer:Int, inputs:Vector[Int]): Behavior[ProgramMessage] = {
-        val instruction = code(pointer).toString
-        val opcode = instruction.reverse.take(2).reverse.toInt
-        val (firstParamMode, secondParamMode, thirdParamMode) = instruction.reverse.drop(2).toVector.map(_.toInt - 48) match {
-          case Vector() => (0, 0, 0)
-          case Vector(a) => (a, 0, 0)
-          case Vector(a, b) => (a, b, 0)
-          case Vector(a, b, c) => (c, b, a)
-        }
-        opcode match {
-          case 1 =>
-            val firstParam = readValue(code, pointer + 1, firstParamMode)
-            val secondParam = readValue(code, pointer + 2, secondParamMode)
-            val newProgram = writeValue(code, pointer + 3, thirdParamMode, firstParam + secondParam)
-            process(newProgram, replyTo, pointer + 4, inputs)
-          case 2 =>
-            val firstParam = readValue(code, pointer + 1, firstParamMode)
-            val secondParam = readValue(code, pointer + 2, secondParamMode)
-            val newProgram = writeValue(code, pointer + 3, thirdParamMode, firstParam * secondParam)
-            process(newProgram, replyTo, pointer + 4, inputs)
-          case 3 if inputs.isEmpty =>
-            ProgramActor.run(code, replyTo, pointer, inputs) // request for more inputs
-          case 3 =>
-            val address = code(pointer + 1)
-            val extendedProgram = code
-            val newCode = extendedProgram.updated(address, inputs.head)
-            process(newCode, replyTo, pointer + 2, inputs.tail)
-          case 4 =>
-            val outputValue = readValue(code, pointer + 1, firstParamMode)
-            replyTo ! Input(outputValue)
-            process(code, replyTo, pointer + 2, inputs)
-          case 5 =>
-            val firstParam = readValue(code, pointer + 1, firstParamMode)
-            val secondParam = readValue(code, pointer + 2, secondParamMode)
-            if (firstParam != 0)
-              process(code, replyTo, secondParam, inputs)
-            else
-              process(code, replyTo, pointer + 3, inputs)
-          case 6 =>
-            val firstParam = readValue(code, pointer + 1, firstParamMode)
-            val secondParam = readValue(code, pointer + 2, secondParamMode)
-            if (firstParam == 0)
-              process(code, replyTo, secondParam, inputs)
-            else
-              process(code, replyTo, pointer + 3, inputs)
-          case 7 =>
-            val firstParam = readValue(code, pointer + 1, firstParamMode)
-            val secondParam = readValue(code, pointer + 2, secondParamMode)
-            val newProgram = writeValue(code, pointer + 3, thirdParamMode, if (firstParam < secondParam) 1 else 0)
-            process(newProgram, replyTo, pointer + 4, inputs)
-          case 8 =>
-            val firstParam = readValue(code, pointer + 1, firstParamMode)
-            val secondParam = readValue(code, pointer + 2, secondParamMode)
-            val newProgram = writeValue(code, pointer + 3, thirdParamMode, if (firstParam == secondParam) 1 else 0)
-            process(newProgram, replyTo, pointer + 4, inputs)
-          case 99 =>
-            Behaviors.stopped
-        }
-      }
-    }
-
-
-    object EngineActor {
-
-      sealed trait Control
-
-      case class Start(value:Int) extends Control
-      case class Result(value:Int) extends Control
-
-
-      def apply():Behavior[Control] = Behaviors.setup{ context =>
-        Behaviors.receiveMessage {
-          case message: Start =>
-
-            Behaviors.receiveMessage{
-              case Result(value) =>
-                Behaviors.stopped
-            }
-        }
-      }
-    }
-
-
-    val system: ActorSystem[EngineActor.Control] = ActorSystem(EngineActor(), "solver")
-    implicit val ec = system.executionContext
-    system ! EngineActor.Start(0)
-    system.getWhenTerminated
-
-
-
-
-
 
 
     def readValue(program: Vector[Int], paramPos: Int, paramMode: Int): Int = {
@@ -257,91 +149,148 @@ object Day7 {
       }
     }
 
-    def executeProgram(program: Vector[Int], inputs: List[Int], pos: Int, outputs: List[Int] = Nil): (Vector[Int], List[Int]) = {
-      val instruction = program(pos).toString
-      val opcode = instruction.reverse.take(2).reverse.toInt
-      val (firstParamMode, secondParamMode, thirdParamMode) = instruction.reverse.drop(2).toVector.map(_.toInt - 48) match {
-        case Vector() => (0, 0, 0)
-        case Vector(a) => (a, 0, 0)
-        case Vector(a, b) => (a, b, 0)
-        case Vector(a, b, c) => (c, b, a)
-      }
 
-      opcode match {
-        case 1 =>
-          val firstParam = readValue(program, pos + 1, firstParamMode)
-          val secondParam = readValue(program, pos + 2, secondParamMode)
-          val newProgram = writeValue(program, pos + 3, thirdParamMode, firstParam + secondParam)
-          executeProgram(newProgram, inputs, pos + 4, outputs)
-        case 2 =>
-          val firstParam = readValue(program, pos + 1, firstParamMode)
-          val secondParam = readValue(program, pos + 2, secondParamMode)
-          val newProgram = writeValue(program, pos + 3, thirdParamMode, firstParam * secondParam)
-          executeProgram(newProgram, inputs, pos + 4, outputs)
-        case 3 =>
-          val address = program(pos + 1)
-          val extendedProgram = program
-          val newProgram = extendedProgram.updated(address, inputs.head)
-          executeProgram(newProgram, inputs.tail, pos + 2, outputs)
-        case 4 =>
-          val outputValue = readValue(program, pos + 1, firstParamMode)
-          executeProgram(program, inputs, pos + 2, outputValue :: outputs)
-        case 5 =>
-          val firstParam = readValue(program, pos + 1, firstParamMode)
-          val secondParam = readValue(program, pos + 2, secondParamMode)
-          if (firstParam != 0)
-            executeProgram(program, inputs, secondParam, outputs)
-          else
-            executeProgram(program, inputs, pos + 3, outputs)
-        case 6 =>
-          val firstParam = readValue(program, pos + 1, firstParamMode)
-          val secondParam = readValue(program, pos + 2, secondParamMode)
-          if (firstParam == 0)
-            executeProgram(program, inputs, secondParam, outputs)
-          else
-            executeProgram(program, inputs, pos + 3, outputs)
-        case 7 =>
-          val firstParam = readValue(program, pos + 1, firstParamMode)
-          val secondParam = readValue(program, pos + 2, secondParamMode)
-          val newProgram = writeValue(program, pos + 3, thirdParamMode, if (firstParam < secondParam) 1 else 0)
-          executeProgram(newProgram, inputs, pos + 4, outputs)
-        case 8 =>
-          val firstParam = readValue(program, pos + 1, firstParamMode)
-          val secondParam = readValue(program, pos + 2, secondParamMode)
-          val newProgram = writeValue(program, pos + 3, thirdParamMode, if (firstParam == secondParam) 1 else 0)
-          executeProgram(newProgram, inputs, pos + 4, outputs)
-        case 99 => (program, outputs)
-      }
-    }
+    object ProgramActor {
+      sealed trait ProgramMessage
 
-    def execute(program: Vector[Int], inputs: List[Int]): (Vector[Int], List[Int]) = executeProgram(program, inputs, 0)
+      case class Setup(linkedTo:ActorRef[Input], lastResponseTo:ActorRef[EngineActor.Control]) extends ProgramMessage
+      case class Input(value:Int) extends ProgramMessage
 
-    def execute(program: String, inputs: List[Int]): (Vector[Int], List[Int]) = execute(program.split(",").toVector.map(_.toInt), inputs)
-
-
-    def executeAmplify(program: Vector[Int], configurations: Iterable[Int], input: Int): Int = {
-      @tailrec
-      def run(program: Vector[Int], remainingConfigurations: Iterable[Int], input: Int): Int = {
-        remainingConfigurations.headOption match {
-          case None => input // == output of the previous amplifier and so the final result
-          case Some(configuration) =>
-            val inputs = configuration :: input :: Nil
-            val output = execute(program, inputs) match {
-              case (_, results) => results.head
-            }
-            run(program, remainingConfigurations.tail, output)
+      def apply(code: Vector[Int]):Behavior[ProgramMessage] = Behaviors.setup { context => {
+          Behaviors.receiveMessage {
+            case setup: Setup =>
+              run(context, code, setup.linkedTo, setup.lastResponseTo)
+          }
         }
       }
 
-      run(program, configurations, input)
+      def run(context:ActorContext[ProgramMessage], code: Vector[Int], replyTo:ActorRef[Input], lastResponseTo:ActorRef[EngineActor.Control], pointer:Int=0, inputs:Vector[Int]=Vector.empty, lastOutput:Option[Int]=None): Behavior[ProgramMessage] =
+          Behaviors.receiveMessage { case message:Input =>
+            process(context, code, replyTo, lastResponseTo, pointer, inputs :+ message.value, lastOutput)
+        }
+
+      def process(context:ActorContext[ProgramMessage], code: Vector[Int], replyTo:ActorRef[Input], lastResponseTo:ActorRef[EngineActor.Control], pointer:Int, inputs:Vector[Int], lastOutput:Option[Int]): Behavior[ProgramMessage] = {
+        val instruction = code(pointer).toString
+        val opcode = instruction.reverse.take(2).reverse.toInt
+        val (firstParamMode, secondParamMode, thirdParamMode) = instruction.reverse.drop(2).toVector.map(_.toInt - 48) match {
+          case Vector() => (0, 0, 0)
+          case Vector(a) => (a, 0, 0)
+          case Vector(a, b) => (a, b, 0)
+          case Vector(a, b, c) => (c, b, a)
+        }
+        opcode match {
+          case 1 =>
+            val firstParam = readValue(code, pointer + 1, firstParamMode)
+            val secondParam = readValue(code, pointer + 2, secondParamMode)
+            val newProgram = writeValue(code, pointer + 3, thirdParamMode, firstParam + secondParam)
+            process(context, newProgram, replyTo, lastResponseTo, pointer + 4, inputs, lastOutput)
+          case 2 =>
+            val firstParam = readValue(code, pointer + 1, firstParamMode)
+            val secondParam = readValue(code, pointer + 2, secondParamMode)
+            val newProgram = writeValue(code, pointer + 3, thirdParamMode, firstParam * secondParam)
+            process(context, newProgram, replyTo, lastResponseTo, pointer + 4, inputs, lastOutput)
+          case 3 if inputs.isEmpty =>
+            ProgramActor.run(context, code, replyTo, lastResponseTo, pointer, inputs, lastOutput) // request for more inputs
+          case 3 =>
+            val address = code(pointer + 1)
+            val extendedProgram = code
+            val newCode = extendedProgram.updated(address, inputs.head)
+            process(context, newCode, replyTo, lastResponseTo, pointer + 2, inputs.tail, lastOutput)
+          case 4 =>
+            val outputValue = readValue(code, pointer + 1, firstParamMode)
+            replyTo ! Input(outputValue)
+            process(context, code, replyTo, lastResponseTo, pointer + 2, inputs, Some(outputValue))
+          case 5 =>
+            val firstParam = readValue(code, pointer + 1, firstParamMode)
+            val secondParam = readValue(code, pointer + 2, secondParamMode)
+            if (firstParam != 0)
+              process(context, code, replyTo, lastResponseTo, secondParam, inputs, lastOutput)
+            else
+              process(context, code, replyTo, lastResponseTo, pointer + 3, inputs, lastOutput)
+          case 6 =>
+            val firstParam = readValue(code, pointer + 1, firstParamMode)
+            val secondParam = readValue(code, pointer + 2, secondParamMode)
+            if (firstParam == 0)
+              process(context, code, replyTo, lastResponseTo, secondParam, inputs, lastOutput)
+            else
+              process(context, code, replyTo, lastResponseTo, pointer + 3, inputs, lastOutput)
+          case 7 =>
+            val firstParam = readValue(code, pointer + 1, firstParamMode)
+            val secondParam = readValue(code, pointer + 2, secondParamMode)
+            val newProgram = writeValue(code, pointer + 3, thirdParamMode, if (firstParam < secondParam) 1 else 0)
+            process(context, newProgram, replyTo, lastResponseTo, pointer + 4, inputs, lastOutput)
+          case 8 =>
+            val firstParam = readValue(code, pointer + 1, firstParamMode)
+            val secondParam = readValue(code, pointer + 2, secondParamMode)
+            val newProgram = writeValue(code, pointer + 3, thirdParamMode, if (firstParam == secondParam) 1 else 0)
+            process(context, newProgram, replyTo, lastResponseTo, pointer + 4, inputs, lastOutput)
+          case 99 =>
+            lastResponseTo ! EngineActor.Result(lastOutput, context.self)
+            Behaviors.stopped
+        }
+      }
     }
 
-    def amplify(program: Vector[Int], input: Int): Int = {
+
+    object EngineActor {
+
+      sealed trait Control
+
+      case class Start(code:Vector[Int], value:Int, configurations:Iterable[Int]) extends Control
+      case class Result(value:Option[Int], from:ActorRef[ProgramActor.ProgramMessage]) extends Control
+
+
+      def apply():Behavior[Control] = Behaviors.setup{ context =>
+        Behaviors.receiveMessage {
+          case Start(code, value, configurations) =>
+            // Create processing actors
+            val programActors = configurations.zipWithIndex.map{ case (config, position) =>
+              val actorName = "system-"+configurations.mkString+"-"+position
+              context.spawn(ProgramActor(code), actorName)
+            }.toList
+            // Send setup message
+            programActors.zip(programActors.tail:+ programActors.head).foreach{ case (leftActor, rightActor) =>
+              leftActor ! ProgramActor.Setup(rightActor, context.self)
+            }
+            // Send actor configuration input
+            programActors.zip(configurations).foreach {case (programActorRef, config) =>
+              programActorRef ! ProgramActor.Input(config)
+            }
+            // Send first actor "boot" input
+            programActors.head ! ProgramActor.Input(value)
+
+            // last programActor from which we are interested by final result
+            val lastProgramActorRef = programActors.last
+
+            Behaviors.receiveMessage{
+              case Result(value, from) if from == lastProgramActorRef =>
+                println(s"***$value*** for ${configurations.mkString("-")}")
+                Behaviors.stopped
+              case Result(value, from) => // we don't care about them
+                //println(s"!!!!! ***$value*** for ${configurations.mkString("-")} from ${from.path}")
+                Behaviors.same
+            }
+        }
+      }
+    }
+
+
+
+
+    def executeAmplify(code: Vector[Int], configurations: Iterable[Int], input: Int): Int = {
+      val system: ActorSystem[EngineActor.Control] = ActorSystem(EngineActor(), "solver")
+      implicit val ec = system.executionContext
+      system ! EngineActor.Start(code, 0, configurations)
+      val future = system.whenTerminated
+      Await.ready(future, 30.seconds)
+      42
+    }
+
+    def amplify(code: Vector[Int], input: Int): Int = {
       val results: Iterator[Int] = for {
         permutation <- 5.to(9).permutations
       } yield {
-        println(permutation)
-        executeAmplify(program, permutation, input)
+        executeAmplify(code, permutation, input)
       }
       results.max
     }
@@ -353,6 +302,5 @@ object Day7 {
       amplify(inputFile.contentAsString, 0)
     }
   }
-*/
 
 }
