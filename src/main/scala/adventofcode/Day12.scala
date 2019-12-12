a668779deeacdf08f2619b8b1bbfc1078e33d617
David Crosson <david.crosson@orange.com>
2019-12-12 15:14:54 +0100

    Day12 Part1 OK

--- src/main/scala/adventofcode/Day12.scala
+++ src/main/scala/adventofcode/Day12.scala
@@ -6,33 +6,42 @@ import scala.math._
 object Day12 {
   object Part1 {
 
-    case class Vect(x:Double, y:Double, z:Double) {
+    case class Vect(x:Long, y:Long, z:Long) {
       def add(that:Vect):Vect = Vect(x+that.x, y+that.y, z+that.z)
+      def absolutesSum=abs(x)+abs(y)+abs(z)
     }
 
-    case class Moon(id:Int, position:Vect, velocity: Vect)
+    case class Moon(id:Int, position:Vect, velocity: Vect) {
+      def move(newVelocity:Vect) = Moon(id, position.add(newVelocity), newVelocity)
+      def pot = position.absolutesSum
+      def kin = velocity.absolutesSum
+      def totalEnergy = pot*kin
+    }
 
     def gravityImpactFor(that: Moon, becauseOfThis: Moon): Vect = {
-      def impact(v1:Double,v2:Double):Double = if (v1 > v2 ) +1d else if (v1 < v2) -1d else 0d
+      def impact(v1:Long,v2:Long):Long = if (v1 < v2 ) +1 else if (v1 > v2) -1 else 0
       Vect(
-        x=impact(that.velocity.x, becauseOfThis.velocity.x),
-        y=impact(that.velocity.y, becauseOfThis.velocity.y),
-        z=impact(that.velocity.z, becauseOfThis.velocity.z),
+        x=impact(that.position.x, becauseOfThis.position.x),
+        y=impact(that.position.y, becauseOfThis.position.y),
+        z=impact(that.position.z, becauseOfThis.position.z),
       )
     }
 
-    def simulate(moons:List[Moon]): Unit = {
-      // update velocity by applying gravity
-      val velocityChanges =
-        moons
-          .combinations(2)
-          .toList
-          .flatMap{case List(a,b) => List(a->gravityImpactFor(a,b), b->gravityImpactFor(b,a)) }
-          .groupMapReduce{case (m, _)=> m}{case (_,c) => c}{ (c1, c2) => c1.add(c2)}
+    def simulate(moons:List[Moon]): Long = {
+      var currentMoons:List[Moon] = moons
+      (1 to 1000) map {  step =>
+        // update velocity by applying gravity
+        val velocityChanges =
+          currentMoons
+            .combinations(2)
+            .toList
+            .flatMap { case List(a, b) => List(a -> gravityImpactFor(a, b), b -> gravityImpactFor(b, a)) }
+            .groupMapReduce { case (m, _) => m } { case (_, c) => c } { (c1, c2) => c1.add(c2) }
 
-      // update position by applying velocity
-
-      // increase time step by one
+        // update position by applying velocity
+        currentMoons = velocityChanges.map { case (moon, changes) => moon.move(moon.velocity.add(changes)) }.toList
+      }
+      currentMoons.map(_.totalEnergy).sum
     }
 
     def parse(input:String):List[Moon] = {
@@ -41,7 +50,7 @@ object Day12 {
         .split("\n")
         .map(_.split("""\s*,\s*"""))
         .zipWithIndex
-        .collect{case (Array(xs,ys,zs), id)=> Moon(id, Vect(xs.toDouble, ys.toDouble, zs.toDouble), Vect(0,0,0))}
+        .collect{case (Array(xs,ys,zs), id)=> Moon(id, Vect(xs.toLong, ys.toLong, zs.toLong), Vect(0,0,0))}
         .toList
     }
     def fileToString(inputFile: File = "data" / "day12" / "input.txt"): String = inputFile.contentAsString
--- src/test/scala/adventofcode/Day12Test.scala
+++ src/test/scala/adventofcode/Day12Test.scala
@@ -5,7 +5,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class Day12Test extends FlatSpec with Matchers {
   import Day12.Part1._
 
-  "input parser" should "" in {
+  "parser" should "parse input instructions" in {
     parse("<x=6, y=10, z=42>") shouldBe List(
       Moon(0, Vect(6,10,42),Vect(0,0,0))
     )
@@ -13,5 +13,11 @@ class Day12Test extends FlatSpec with Matchers {
       Moon(0,Vect(6,10,42),Vect(0,0,0)),
       Moon(1, Vect(42,24,4),Vect(0,0,0)),
       )
+  }
+  "simulation" should "return the right result" in {
+    simulate(parse(fileToString())) should not be 78078410000L //too high
+    simulate(parse(fileToString())) should not be 156410 //too high
+    simulate(parse(fileToString())) should not be 395 //too low
+    simulate(parse(fileToString())) shouldBe 13045
   }
 }
