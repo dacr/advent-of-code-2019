4af9fc6699f159c7d6b4562605c20704270b8c49
Crosson David <crosson.david@gmail.com>
2019-12-12 19:07:34 +0100

    Better than previous one but not enough

--- src/main/scala/adventofcode/Day12.scala
+++ src/main/scala/adventofcode/Day12.scala
@@ -6,10 +6,9 @@ import scala.math._
 object Day12 {
   object Part1 {
 
-    case class Vect(x:Int, y:Int, z:Int) {
-      def add(that:Vect):Vect = Vect(x+that.x, y+that.y, z+that.z)
+    class Vect(val x:Int, val y:Int, val z:Int) {
+      def add(that:Vect):Vect = new Vect(x+that.x, y+that.y, z+that.z)
       def absolutesSum: Int =abs(x)+abs(y)+abs(z)
-      override def toString: String = s"$x/$y/$z"
     }
 
     case class Moon(id:Int, position:Vect, velocity: Vect) {
@@ -17,12 +16,13 @@ object Day12 {
       def pot = position.absolutesSum
       def kin = velocity.absolutesSum
       def totalEnergy = pot*kin
-      override def toString: String = s"$id-$position$velocity"
+
+      val hash = 31*(id + 31L*(position.x + 31L*(position.y + 31L*(position.z + 31L*(velocity.x + 31L*(velocity.y + 31L*velocity.z))))))
     }
 
     def gravityImpactFor(that: Moon, becauseOfThis: Moon): Vect = {
       def impact(v1:Int,v2:Int):Int = if (v1 < v2 ) +1 else if (v1 > v2) -1 else 0
-      Vect(
+      new Vect(
         x=impact(that.position.x, becauseOfThis.position.x),
         y=impact(that.position.y, becauseOfThis.position.y),
         z=impact(that.position.z, becauseOfThis.position.z),
@@ -46,42 +46,28 @@ object Day12 {
       currentMoons.map(_.totalEnergy).sum
     }
 
-    //val primes = List(127, 131, 137, 139, 149, 151, 157, 163, 167, 173)
-    case class MoonList(moons:List[Moon]) {
-      //val hash = moons.zip(primes).map{ case (moon,prime) => moon.id*moon.hash*prime}.sum
-      //val hash:Int = scala.util.hashing.MurmurHash3.stringHash(toString)
-      //val hash:Int = scala.util.hashing.MurmurHash3.listHash(moons, 42)
-      override def toString: String = moons.map(_.toString).mkString(",")
-      val hash:Long = toString().foldLeft(0L) { case (code, c) => 31*code + c }
-    }
+    def moonsHash(moons:List[Moon]):Long = moons.foldLeft(0L) { case (code, c) => 31*code + c.hash }
+
 
     def howManyStepsToGoBackToAnAlreadySeenState(moons:List[Moon]): Long = {
       //var knownStates = Set.empty[Long]
 
-      val referenceMoons = MoonList(moons)
-      var currentMoons:MoonList = MoonList(moons)
+      val referenceHash = moonsHash(moons)
+      var currentMoons:List[Moon] = moons
+      var currentHash:Long = -1
       var steps = 0L
       println("START")
-      //while(! knownStates.contains(currentMoons.hash))  {
-      while(currentMoons.hash != referenceMoons)  {
-        //knownStates += currentMoons.hash
-        if (steps % 100000 == 0) println(steps)
-        // update velocity by applying gravity
-//        val velocityChanges =
-//          currentMoons.moons
-//            .combinations(2)
-//            .toList
-//            .flatMap { case List(a, b) => List(a -> gravityImpactFor(a, b), b -> gravityImpactFor(b, a)) }
-//            .groupMapReduce { case (m, _) => m } { case (_, c) => c } { (c1, c2) => c1.add(c2) }
+      while(currentHash != referenceHash)  {
+        if (steps % 1000000L == 0L) println(steps)
         val velocityChanges =
-          currentMoons.moons
+          currentMoons
             .combinations(2)
             .toList
             .flatMap { case List(a, b) => List(a -> gravityImpactFor(a, b), b -> gravityImpactFor(b, a)) }
             .groupMapReduce { case (m, _) => m } { case (_, c) => c } { (c1, c2) => c1.add(c2) }
 
-        // update position by applying velocity
-        currentMoons = MoonList(velocityChanges.map { case (moon, changes) => moon.move(moon.velocity.add(changes)) }.toList)
+        currentMoons = velocityChanges.map { case (moon, changes) => moon.move(moon.velocity.add(changes)) }.toList
+        currentHash = moonsHash(currentMoons)
         steps+=1L
       }
       steps
@@ -94,7 +80,7 @@ object Day12 {
         .split("\n")
         .map(_.split("""\s*,\s*"""))
         .zip(primes)
-        .collect{case (Array(xs,ys,zs), id)=> Moon(id, Vect(xs.toInt, ys.toInt, zs.toInt), Vect(0,0,0))}
+        .collect{case (Array(xs,ys,zs), id)=> Moon(id, new Vect(xs.toInt, ys.toInt, zs.toInt), new Vect(0,0,0))}
         .toList
     }
     def fileToString(inputFile: File = "data" / "day12" / "input.txt"): String = inputFile.contentAsString
--- src/test/scala/adventofcode/Day12Test.scala
+++ src/test/scala/adventofcode/Day12Test.scala
@@ -5,15 +5,6 @@ import org.scalatest.{FlatSpec, Matchers}
 class Day12Test extends FlatSpec with Matchers {
   import Day12.Part1._
 
-  "parser" should "parse input instructions" in {
-    parse("<x=6, y=10, z=42>") shouldBe List(
-      Moon(3, Vect(6,10,42),Vect(0,0,0))
-    )
-    parse("<x=6, y=10, z=42>\n<x=42, y=24, z=4>") shouldBe List(
-      Moon(3,Vect(6,10,42),Vect(0,0,0)),
-      Moon(5, Vect(42,24,4),Vect(0,0,0)),
-      )
-  }
   "simulation" should "return the right result" in {
     simulate(parse(fileToString())) should not be 156410 //too high
     simulate(parse(fileToString())) should not be 395 //too low
