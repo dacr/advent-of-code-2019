3f3307cd20f63b6a808007c923aaf5533cf836be
Crosson David <crosson.david@gmail.com>
2019-12-14 23:23:54 +0100

    Day14 Part2 is OK

--- src/main/scala/adventofcode/day14/Day14.scala
+++ src/main/scala/adventofcode/day14/Day14.scala
@@ -52,8 +52,9 @@ object Day14 {
     new NanoFactory(reactions)
   }
 
-  object Part1 {
 
+
+  object Part1 {
 
     def divideRoundUp(x: Int, y: Int):Int = {
       x / y + (if (x % y > 0) 1 else 0)
@@ -97,5 +98,73 @@ object Day14 {
       val nanoFactory = parseInput(chemistry)
       howManyOre(nanoFactory, Fuel, 1)
     }
+  }
+
+
+
+  object Part2 {
+
+    def divideRoundUp(x: Long, y: Long):Long = {
+      x / y + (if (x % y > 0L) 1L else 0L)
+    }
+
+
+    var stocks = collection.mutable.Map.empty[Chemical, Long]   // no stocks, remaining chemical because reaction produce too much
+
+    def howManyOre(nanoFactory: NanoFactory, chemical: Chemical, requirement:Long): Option[Long] = {
+      def worker(goal:Chemical, requirement:Long):Option[Long] = {
+        nanoFactory.reactionForChemical.get(goal).map { reaction =>
+          val adjustedRequirement = stocks.get(goal) match {
+            case None =>
+              requirement
+            case Some(stock) if stock <= requirement =>
+              stocks(goal) = 0
+              requirement - stock
+            case Some(stock) if stock > requirement =>
+              stocks(goal) = (stock - requirement)
+              0
+          }
+          if (adjustedRequirement == 0L) 0L
+          else {
+            val (factor, unused) = {
+              if (adjustedRequirement <= reaction.output.quantity)
+                (1L, reaction.output.quantity - adjustedRequirement)
+              else {
+                val f = divideRoundUp(adjustedRequirement, reaction.output.quantity)
+                (f, reaction.output.quantity * f - adjustedRequirement)
+              }
+            }
+            if (unused > 0L) stocks(goal) =  stocks.get(goal).getOrElse(0L) + unused
+            val (ores, notOres) = reaction.inputs.partition(_.chemical == Ore)
+            ores.map(_.quantity.toLong * factor).sum + notOres.flatMap(ingredient => worker(ingredient.chemical, factor * ingredient.quantity.toLong)).sum
+          }
+        }
+      }
+      val result = worker(chemical, requirement)
+      //println(stocks)
+      result
+    }
+
+    def bestSolution(chemistry:String):Long = {
+      val nanoFactory = parseInput(chemistry)
+      val oresGoal = 1000000000000L
+      var remaining = oresGoal
+      var fuelProduced = 0L
+      while(remaining > 0L) {
+        if (fuelProduced % 100_000L == 0L) println(remaining)
+        val requiredOres = howManyOre(nanoFactory, Fuel, 1L).getOrElse(0L)
+        remaining -= requiredOres
+        if (remaining > 0) fuelProduced += 1
+      }
+      fuelProduced
+    }
+
+
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    import Part2._
+    bestSolution(fileToString())
   }
 }
