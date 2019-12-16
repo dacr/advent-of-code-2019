c199423c874234843b7290342765f77f7c1ba2d8
Crosson David <crosson.david@gmail.com>
2019-12-16 21:51:16 +0100

    Faster but not enough faster

--- src/main/scala/adventofcode/Day16.scala
+++ src/main/scala/adventofcode/Day16.scala
@@ -2,10 +2,13 @@ package adventofcode
 
 import better.files._
 
+import scala.annotation.tailrec
+
 object Day16 {
   def fileToString(inputFile: File = "data" / "day16" / "input.txt"): String = inputFile.contentAsString
 
   val basePatternDefault = Array(0, 1, 0, -1)
+  val basePatternDefaultSize = basePatternDefault.length
 
   object Part1 {
     def process(input: String, iterations: Int = 100): String = {
@@ -35,7 +38,6 @@ object Day16 {
           .toList
 
       def worker(currentDigits: List[Int], currentIteration: Int): List[Int] = {
-        println(s"--- iteration $currentIteration ---")
         if (currentIteration == iterations) currentDigits
         else {
           val newDigits = patterns.map { pattern => lastDigit(currentDigits.zip(pattern.tail).map { case (a, b) => a * b }.sum) }
@@ -47,12 +49,53 @@ object Day16 {
     }
 
   }
+
   object Part2 {
 
-    def main(args: Array[String]): Unit = {
-      //val result = process(fileToString() * 10000)
-      //println(result)
+    @inline def lastDigit(value: Int): Int = math.abs(value % 10)
+
+
+    @inline def patternGenerator(goalPosition:Int, index:Int):Int = {
+      val i = index / goalPosition
+      basePatternDefault(i % basePatternDefaultSize)
     }
+
+    def process(input: String, iterations: Int = 100): String = {
+      processDigits(input.map(_.toInt - 48).toArray, iterations).take(8).mkString("")
+    }
+
+    def processDigits(digits: Array[Int], iterations: Int): Array[Int] = {
+      val digitsCount = digits.length
+      var sum=0
+      @tailrec
+      def worker(digits1: Array[Int], digits2:Array[Int], currentIteration: Int): Array[Int] = {
+        println(s"--- $currentIteration ---")
+        if (currentIteration == iterations) digits1
+        else {
+          var goalPosition=1
+          while( goalPosition <= digitsCount ) {
+            sum = 0
+            var index = 0
+            while (index < digitsCount) {
+              sum += digits1(index) * patternGenerator(goalPosition, index + 1)
+              index+=1
+            }
+            digits2(goalPosition-1) = lastDigit(sum)
+            goalPosition+=1
+          }
+          worker(digits2, digits1, currentIteration + 1)
+        }
+      }
+
+      worker(digits, digits.clone(), 0)
+    }
+
+
+    }
+  def main(args: Array[String]): Unit = {
+    import Part2._
+    val result = process(fileToString() * 10000)
+    println(result)
   }
 }
 
--- src/test/scala/adventofcode/Day16Test.scala
+++ src/test/scala/adventofcode/Day16Test.scala
@@ -5,11 +5,12 @@ import org.scalatest.{Matchers, WordSpec}
 class Day16Test extends WordSpec with Matchers {
 
   "Day16" should {
+    import Day16._
 
     "Part1" should {
       import Day16.Part1._
       "makePattern" should {
-        "give the same value for pos 0" in {
+        "give the right value for given samples" in {
           makePattern(1, 4) shouldBe List(0, 1, 0, -1)
           makePattern(1, 8) shouldBe List(0, 1, 0, -1, 0, 1, 0, -1)
           makePattern(2, 5) shouldBe List(0, 0, 1, 1, 0)
@@ -23,15 +24,34 @@ class Day16Test extends WordSpec with Matchers {
         process("69317163492948606335995924319873") shouldBe "52432133"
       }
       "give the right result with the provided file" in {
-        //process(fileToString()) shouldBe "42205986"
+        process(fileToString()) shouldBe "42205986"
       }
     }
 
     "Part2" should {
       import Day16.Part2._
-      "should run quickly" in {
-        succeed
+      "pattern generator" should {
+        "give the right value for given samples" in {
+          0.until(4).map(n => patternGenerator(1, n)) shouldBe List(0, 1, 0, -1)
+          0.until(8).map(n => patternGenerator(1, n)) shouldBe List(0, 1, 0, -1, 0, 1, 0, -1)
+          0.until(4).map(n => patternGenerator(2, n)) shouldBe List(0, 0, 1, 1)
+        }
       }
+
+      "should run quickly on previous examples" in {
+        process("12345678", 4) shouldBe "01029498"
+        process("80871224585914546619083218645595") shouldBe "24176176"
+        process("19617804207202209144916044189917") shouldBe "73745418"
+        process("69317163492948606335995924319873") shouldBe "52432133"
+      }
+      "give the right result with the provided file" in {
+        process(fileToString()) shouldBe "42205986"
+      }
+
+      "give the right result with the provided file * 10000" in {
+        process(fileToString()) shouldBe "42205986"
+      }
+
     }
   }
 }
