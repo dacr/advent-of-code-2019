af888f17e4091c7ccf5768c98c903dc2914a18a0
Crosson David <crosson.david@gmail.com>
2019-12-17 22:50:24 +0100

    Day17 Part2 - compute full path algo done

--- src/main/scala/adventofcode/Day17.scala
+++ src/main/scala/adventofcode/Day17.scala
@@ -10,6 +10,8 @@ object Day17 {
 
   class Code(val program: Vector[BigInt]) {
 
+    def change(address: Int, value: BigInt):Code = new Code(program.updated(address, value))
+
     private def updated(pointer: Int, value: BigInt): Code = {
       val adjustedProgram =
         if (pointer < program.size) program
@@ -162,6 +164,7 @@ object Day17 {
           process(context, code, outputToOpt, lastResponseToOpt, pointer + 2, inputs, outputs, newRelativeBase)
         case 99 =>
           lastResponseToOpt.foreach { lastResponseTo => lastResponseTo ! AftScaffoldingControlActor.Result(outputs, code, context.self) }
+          println("FINISHED - 99")
           Behaviors.stopped
       }
     }
@@ -178,39 +181,56 @@ object Day17 {
   object Intersection extends Tile {
     override def toString = "O"
   }
-  object BotUp extends Tile {
+  trait Bot extends Tile {
+    val direction:Direction
+  }
+  object BotUp extends Bot {
     override def toString = "^"
+    override val direction: Direction = Up
   }
-  object BotDown extends Tile {
+  object BotDown extends Bot {
     override def toString = "v"
+    override val direction: Direction = Down
   }
-  object BotLeft extends Tile {
+  object BotLeft extends Bot {
     override def toString = "<"
+    override val direction: Direction = Left
   }
-  object BotRight extends Tile {
+  object BotRight extends Bot {
     override def toString = ">"
+    override val direction: Direction = Right
   }
 
   type Position = (Int,Int)
 
   sealed trait Direction {
     val reverse: Direction
+    val turnLeft: Direction
+    val turnRight: Direction
   }
 
   object Up extends Direction {
     val reverse = Down
+    val turnLeft = Left
+    val turnRight = Right
   }
 
   object Down extends Direction {
     val reverse = Up
+    val turnLeft = Right
+    val turnRight = Left
   }
 
   object Left extends Direction {
     val reverse = Right
+    val turnLeft = Down
+    val turnRight = Up
   }
 
   object Right extends Direction {
     val reverse = Left
+    val turnLeft = Up
+    val turnRight = Down
   }
 
   val directions = List(Up,Down,Left,Right)
@@ -231,6 +251,30 @@ object Day17 {
     val width = zones.head.size
     val height = zones.size
 
+    override def toString() = zones.map(_.mkString).mkString("\n")
+
+    def get(pos: Position): Tile = pos match {
+      case (x, y) => zones(y)(x)
+    }
+
+    def safeGet(pos:Position):Option[Tile] = pos match {
+      case (x,y) if x < 0 || y < 0 || x > width-1 || y > height-1 => None
+      case (x,y) => Some(zones(y)(x))
+    }
+
+    def set(pos: Position, tile: Tile): Land = pos match {
+      case (x, y) => Land(zones.updated(y, zones(y).updated(x, tile)))
+    }
+
+    def search(criteria: Tile=>Boolean):IndexedSeq[(Tile,Position)] = {
+      for {
+        x <- 0 until width
+        y <- 0 until height
+        pos = (x, y)
+        tile = get(pos)
+        if criteria(tile)
+      } yield tile->pos
+    }
     def intersections():List[Position] = {
       val results = for {
         x <- 1 until width-1
@@ -240,21 +284,69 @@ object Day17 {
       } yield pos
       results.toList
     }
-
-    override def toString() = zones.map(_.mkString).mkString("\n")
-
-    def get(pos: Position): Tile = pos match {
-      case (x, y) => zones(y)(x)
-    }
-
-    def set(pos: Position, tile: Tile): Land = pos match {
-      case (x, y) => Land(zones.updated(y, zones(y).updated(x, tile)))
+    def botPosition():Option[(Tile,Position)] = {
+      search(tile => tile != OpenSpace && tile!=Scaffold).headOption
     }
   }
 
   object Land {
+    def charToTile(value: Char):Tile = value match {
+      case '.' => OpenSpace
+      case '#' => Scaffold
+      case '^' => BotUp
+      case 'v' => BotDown
+      case '<' => BotLeft
+      case '>' => BotRight
+      }
+
     def apply(width: Int, height: Int): Land = {
       Land(Vector.fill(height)(Vector.fill(width)(OpenSpace)))
+    }
+    def apply(landString: String): Land = {
+      Land(
+        landString
+          .split("\n")
+          .toVector
+          .map(row => row.toCharArray.toVector.map(charToTile))
+      )
+    }
+  }
+
+
+
+  sealed trait Turn
+  object TurnLeft extends Turn {
+    override def toString: String = "L"
+  }
+  object TurnRight extends Turn {
+    override def toString: String = "R"
+  }
+  case class MoveInstruction(turn:Turn,moveCount:Int)
+
+  def fullPath(land:Land):List[MoveInstruction] = {
+    land.botPosition() match {
+      case None => List.empty
+      case Some((botTile:Bot, initialPosition)) =>
+        def walker(direction:Direction, position:Position, pathAccumulator:List[MoveInstruction]):List[MoveInstruction] = {
+          def movesSimulation(direction:Direction, turned:Turn): Option[(Position, MoveInstruction)] = {
+            var count = 0
+            var currentPosition = position
+            while (land.safeGet(move(currentPosition)(direction)) == Some(Scaffold)) {
+              currentPosition = move(currentPosition)(direction)
+              count += 1
+            }
+            if (count==0) None else Some(currentPosition->MoveInstruction(turned, count))
+          }
+          val leftForward = movesSimulation(direction.turnLeft, TurnLeft)
+          val rightForward = movesSimulation(direction.turnRight, TurnRight)
+          leftForward.orElse(rightForward) match {
+            case None => pathAccumulator.reverse // Finished
+            case Some((newPosition, instruction)) =>
+              val newDirection = if (instruction.turn == TurnLeft) direction.turnLeft else direction.turnRight
+              walker(newDirection, newPosition, instruction::pathAccumulator)
+          }
+        }
+        walker(botTile.direction,initialPosition, Nil)
     }
   }
 
@@ -273,10 +365,8 @@ object Day17 {
     object NeedInput extends Control
 
 
-    def computeIntersectionsSum(programActor: ActorRef[ProgramActor.ProgramMessage], listenActor: ActorRef[ListenActor.Response], land: Land): Behavior[Control] = {
-      val value = land.intersections().map{case (x,y) => x*y}.sum
-      listenActor ! ListenActor.Response(value)
-      Behaviors.stopped
+    def part2(programActor: ActorRef[ProgramActor.ProgramMessage], listenActor: ActorRef[ListenActor.Response], land: Land): Behavior[Control] = {
+      ???
     }
 
     def buildLand(programActor: ActorRef[ProgramActor.ProgramMessage], listenActor: ActorRef[ListenActor.Response], landTiles: Vector[Vector[Tile]]): Behavior[Control] = {
@@ -284,19 +374,15 @@ object Day17 {
         case response:Output if response.value == 10 && landTiles.last.isEmpty =>
           val land = Land(landTiles.init)
           println(land)
-          computeIntersectionsSum(programActor, listenActor, land)
+          val value = land.intersections().map{case (x,y) => x*y}.sum
+          listenActor ! ListenActor.Response(value)
+
+          part2(programActor, listenActor, land)
         case response:Output if response.value == 10 =>
           val newLandTiles = landTiles.appended(Vector.empty[Tile])
           buildLand(programActor, listenActor, newLandTiles)
         case response:Output =>
-          val tile = response.value.toChar match {
-            case '.' => OpenSpace
-            case '#' => Scaffold
-            case '^' => BotUp
-            case 'v' => BotDown
-            case '<' => BotLeft
-            case '>' => BotRight
-          }
+          val tile = Land.charToTile(response.value.toChar)
           val newLandTiles = landTiles.init :+ (landTiles.last:+tile)
           buildLand(programActor, listenActor, newLandTiles)
         case response:Result =>
--- src/test/scala/adventofcode/Day17Test.scala
+++ src/test/scala/adventofcode/Day17Test.scala
@@ -2,6 +2,7 @@ package adventofcode
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import org.scalatest.WordSpecLike
+import org.scalatest.OptionValues._
 
 class Day17Test extends ScalaTestWithActorTestKit with WordSpecLike {
 
@@ -22,16 +23,54 @@ class Day17Test extends ScalaTestWithActorTestKit with WordSpecLike {
             Vector(o,o,S,S,S,S,S,o,o,o,S,o,o),
           ))
           land.intersections().map{case(x,y) => x*y}.sum shouldBe 76
-
+        }
+        "possible to be created from String" in {
+          val landString =
+            """..#..........
+              |..#..........
+              |#######...###
+              |#.#...#...#.#
+              |#############
+              |..#...#...#..
+              |..#####...^..""".stripMargin
+          val land = Land(landString)
+          land.intersections().map{case(x,y) => x*y}.sum shouldBe 76
+          land.botPosition().value shouldBe (BotUp,(10,6))
         }
       }
-      "Part1 & 2" should {
+      "paths" should {
+        "be detected from land" in {
+          val landString =
+            """...###
+              |...#.#
+              |######
+              |#..#..
+              |#..<..""".stripMargin
+          val land = Land(landString)
+          val M = MoveInstruction
+          val L = TurnLeft
+          val R = TurnRight
+          fullPath(land) shouldBe List(M(R,4),M(R,2),M(R,2),M(R,5),M(L,2))
+        }
+      }
+      "Part1" should {
         "work with the provided input file" in {
           val listenProbe = createTestProbe[ListenActor.Response]()
           val code = fileToCode()
           val driverBotActor = spawn(AftScaffoldingControlActor(code, listenProbe.ref))
           val response1 = listenProbe.expectMessageType[ListenActor.Response]
           response1.value shouldBe 8520 // Part1
+        }
+      }
+      "Part2" should {
+        "work with the provided input file" in {
+          val listenProbe = createTestProbe[ListenActor.Response]()
+          val code = fileToCode().change(0,2)
+          val driverBotActor = spawn(AftScaffoldingControlActor(code, listenProbe.ref))
+          val response1 = listenProbe.expectMessageType[ListenActor.Response]
+          response1.value shouldBe 8520 // Part1
+          val response2 = listenProbe.expectMessageType[ListenActor.Response]
+          response2.value shouldBe 42L // Part2
         }
       }
     }
